6acecd42de88021e0290dccca8bda4076a2e2191
theRingleman <sam.ringleman@gmail.com>
2016-05-01 19:20:22 -0600

    Start scraping process and switched up website for the time being, not as comprehensive of a site but a whole lot more breweries and towns.

--- lib/noco_breweries/scraper.rb
+++ lib/noco_breweries/scraper.rb
@@ -1,9 +1,15 @@
-# require "pry"
+require "pry"
+require "nokogiri"
+require "open-uri"
+
 
 class NocoBreweries::Scraper
 
   def self.scrape_towns
-
+    doc = Nokogiri::HTML(open("http://www.coloradocraftbrews.com/colorado-breweries/"))
+    # Brewery name and town: doc.css("p strong")[5].text
+    # Brewery Website: doc.css("p strong a").attribute("href").value
+    binding.pry
   end
 
   def self.scrape_breweries
