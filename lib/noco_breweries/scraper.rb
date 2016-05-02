d9345c8b0c302ec92822929d8f1a6f84c1f0cbb8
theRingleman <sam.ringleman@gmail.com>
2016-05-02 10:41:21 -0600

    Started the scraping and add a method for getting the brewery details

--- lib/noco_breweries/scraper.rb
+++ lib/noco_breweries/scraper.rb
@@ -5,15 +5,28 @@ require "open-uri"
 
 class NocoBreweries::Scraper
 
+  def self.site
+    Nokogiri::HTML(open("http://fortcollinsbreweryguide.com/breweries.aspx"))
+  end
+
   def self.scrape_towns
-    doc = Nokogiri::HTML(open("http://www.coloradocraftbrews.com/colorado-breweries/"))
-    # Brewery name and town: doc.css("p strong")[5].text
-    # Brewery Website: doc.css("p strong a").attribute("href").value
-    binding.pry
+    doc = Nokogiri::HTML(open("http://fortcollinsbreweryguide.com/breweries.aspx"))
+    fort_collins = doc.css("#ctl00_MainContent_LocationsRepeater_ctl00_CityLabel").text
+    berthoud = doc.css("#ctl00_MainContent_LocationsRepeater_ctl02_CityLabel").text
+    greeley = doc.css("#ctl00_MainContent_LocationsRepeater_ctl04_CityLabel").text
+    loveland = doc.css("#ctl00_MainContent_LocationsRepeater_ctl06_CityLabel").text
+    windsor = doc.css("#ctl00_MainContent_LocationsRepeater_ctl08_CityLabel").text
   end
 
   def self.scrape_breweries
+    # brewery name: site.css("a h3").text.strip
+    # brewery foco site url: site.css("td.content a") also you have to add fortcollinsbreweryguide.com/ and then the site
 
+    binding.pry
+  end
+
+  def scrape_details #this might take in a brewery object as an argument
+    
   end
 
 end
